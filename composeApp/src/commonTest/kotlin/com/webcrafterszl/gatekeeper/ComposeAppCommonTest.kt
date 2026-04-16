fe93d00879e8dc4f4ea11005e1d85e91d87cdfe3
Leonardo E. Silva <42047532+leoesilva@users.noreply.github.com>
2026-04-16 01:19:07 -0300

    refactor: Alteração do nome do package principal

---
+++ composeApp/src/commonTest/kotlin/com/webcrafterszl/gatekeeper/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.webcrafterszl.gatekeeper
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
